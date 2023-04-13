299c473d3ddd7423f18807860b104307532b56e9
Igor Kupriianov <igorkupriynov@yandex.ru>
2023-04-13 16:33:57 +0300

    Rewriting requirements.txt

--- NotebookPricePrediction/settings.py
+++ NotebookPricePrediction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ftp_$3v#$ea93$h4(g0cjq-!o6(965q_4lx==9spho9djm#lx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
